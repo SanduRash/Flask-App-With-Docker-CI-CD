7910c6d03f44191476321890b702b16174b3ee87
Sanduni Galbokka Hewa <133158652+SanduRash@users.noreply.github.com>
2025-06-11 19:28:35 -0400

    Check if CI/CD find the error

--- static/script.js
+++ static/script.js
@@ -2,7 +2,7 @@ document.addEventListener("DOMContentLoaded", () => {
     fetch("/api")
         .then(res => res.json())
         .then(data => {
-            const msg = document.createElement("p");
+            const msg = d ocument.createElement("p");
             msg.innerText = data.message;
             document.body.appendChild(msg);
         });
